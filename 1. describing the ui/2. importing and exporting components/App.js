24a3c282d43e618926d39d16da9e14d0cefa6674
wowba <leeyuwk54@gmail.com>
2023-09-05 01:39:11 +0900

    describing the ui - importing and exporting components 풀이

---
+++ 1. describing the ui/2. importing and exporting components/App.js
@@ -0,0 +1,11 @@
+import Gallery from './Gallery.js';
+import Profile from './Profile.js';
+
+export default function App() {
+  return (
+    <div>
+      <Gallery />
+      <Profile />
+    </div>
+  );
+}
