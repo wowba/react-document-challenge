1c49efacc258bf679d0cae6f1fc1c9c94622def3
wowba <leeyuwk54@gmail.com>
2023-09-06 17:38:32 +0900

    describing the ui - keeping components pure 풀이

--- 1. describing the ui/8. keeping components pure/challenge3.js
+++ 1. describing the ui/8. keeping components pure/challenge3.js
@@ -1,5 +1,7 @@
 export default function StoryTray({ stories }) {
+  // 순수 함수는 렌더링 전에 존재했던 객체나 변수를 변경하지 않아야 한다.
   // 리액트는 props가 변경되면 리렌더링을 하게 된다.
+  // 화면을 업데이트 하려면 setState를 활용하자.
   const newStories = stories.slice()
 
   newStories.push({
