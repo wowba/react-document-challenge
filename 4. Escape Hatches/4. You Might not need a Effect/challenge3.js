232b596b0db272e16cadb32703aa510a3d0f93c4
wowba <leeyuwk54@gmail.com>
2023-09-23 16:46:07 +0900

    Escape Hatches - You Might not need an Effect 풀이

---
+++ 4. Escape Hatches/4. You Might not need a Effect/challenge3.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react';
+
+export default function EditForm({ savedContact, onSave }) {
+  return (
+    <EditContact
+      key={savedContact.id}
+      savedContact={savedContact}
+      onSave={onSave}
+    />
+  )
+}
+
+function EditContact({ savedContact, onSave }) {
+  const [name, setName] = useState(savedContact.name);
+  const [email, setEmail] = useState(savedContact.email);
+
+  return (
+    <section>
+      <label>
+        Name:{' '}
+        <input
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </label>
+      <label>
+        Email:{' '}
+        <input
+          type="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+      </label>
+      <button onClick={() => {
+        const updatedData = {
+          id: savedContact.id,
+          name: name,
+          email: email
+        };
+        onSave(updatedData);
+      }}>
+        Save
+      </button>
+      <button onClick={() => {
+        setName(savedContact.name);
+        setEmail(savedContact.email);
+      }}>
+        Reset
+      </button>
+    </section>
+  );
+}
